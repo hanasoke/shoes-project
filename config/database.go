7ba5a63bdb36a5e436e94911cad21ea225647ad7
hanasbp <hanasoke@gmail.com>
2025-12-17 08:19:53 +0700

    updated 2 pages

--- config/database.go
+++ config/database.go
@@ -1,1 +1,20 @@
 package config
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var DB *sql.DB
+
+func ConnectDB() {
+	db, err := sql.Open("mysql", "root:@/shoes_store?parseTime=true")
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Database connected")
+	DB = db
+}
