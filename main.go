6baa7e8857ea25acd56675f5b8af33c479653ab9
hanasbp <hanasoke@gmail.com>
2025-12-17 15:00:24 +0700

    updated 2 html pages

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"shoes-project/config"
+	"shoes-project/controllers/brand"
 	"shoes-project/controllers/homepage"
 )
 
@@ -12,6 +13,9 @@ func main() {
 
 	// 1. Homepage
 	http.HandleFunc("/", homepage.Index)
+
+	// 2. Brand
+	http.HandleFunc("/brand", brand.Index)
 
 	log.Println("Server running on port 8080")
 	http.ListenAndServe(":8080", nil)
