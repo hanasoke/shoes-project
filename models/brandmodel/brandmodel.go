55bbc8e22911332b15ec61a21bc9740fcf68a779
hanasbp <hanasoke@gmail.com>
2025-12-19 21:02:50 +0700

    added create function in brand model

--- models/brandmodel/brandmodel.go
+++ models/brandmodel/brandmodel.go
@@ -26,3 +26,21 @@ func GetAll() []entities.Brand {
 
 	return brands
 }
+
+func Create(brand entities.Brand) bool {
+	result, err := config.DB.Exec(`
+		INSERT INTO brands (brand_name, created_at, updated_at) VALUE (?, ?, ?)`,
+		brand.Brand_Name, brand.CreatedAt, brand.UpdatedAt,
+	)
+
+	if err != nil {
+		panic(err)
+	}
+
+	lastInsertId, err := result.LastInsertId()
+	if err != nil {
+		panic(err)
+	}
+
+	return lastInsertId > 0
+}
