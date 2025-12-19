f51dae6a71bde16a1f14c8189df01eaca4b40368
hanasbp <hanasoke@gmail.com>
2025-12-19 20:14:07 +0700

    added get function in brand model

--- models/brand/brand.go
+++ models/brand/brand.go
@@ -1,5 +1,28 @@
 package brand
 
-func GetAll() {
+import (
+	"shoes-project/config"
+	"shoes-project/entities"
+)
 
+func GetAll() []entities.Brand {
+	rows, err := config.DB.Query(`SELECT * FROM brand`)
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	var brands []entities.Brand
+
+	for rows.Next() {
+		var brand entities.Brand
+		if err := rows.Scan(&brand.Brand_Id, &brand.Brand_Name, &brand.CreatedAt, &brand.UpdatedAt); err != nil {
+			panic(err)
+		}
+
+		brands = append(brands, brand)
+	}
+
+	return brands
 }
