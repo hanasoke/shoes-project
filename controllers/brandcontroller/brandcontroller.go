677604a63152bfd86067e2fc4a439b05c9ddd3b6
hanasbp <hanasoke@gmail.com>
2025-12-20 20:36:04 +0700

    updated brandcontroller

--- controllers/brandcontroller/brandcontroller.go
+++ controllers/brandcontroller/brandcontroller.go
@@ -3,7 +3,9 @@ package brandcontroller
 import (
 	"html/template"
 	"net/http"
+	"shoes-project/entities"
 	"shoes-project/models/brandmodel"
+	"time"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
@@ -28,6 +30,40 @@ func Add(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if r.Method == http.MethodPost {
+		brandName := r.FormValue("brand_name")
 
+		// 1️⃣ NULL / empty validation
+		if brandName == "" {
+			data := map[string]any{
+				"error": "Brand name cannot be empty",
+			}
+			temp := template.Must(template.ParseFiles("views/brand/create.html"))
+			temp.Execute(w, data)
+			return
+		}
+
+		brand := entities.Brand{
+			Brand_Name: brandName,
+			CreatedAt:  time.Now(),
+			UpdatedAt:  time.Now(),
+		}
+
+		err := brandmodel.Create(brand)
+		if err != nil {
+			msg := "Failed to create brand"
+
+			if err == brandmodel.ErrDuplicateBrand {
+				msg = "Brand already exists"
+			}
+
+			data := map[string]any{
+				"error": msg,
+			}
+			temp := template.Must(template.ParseFiles("views/brand/create.html"))
+			temp.Execute(w, data)
+			return
+		}
+
+		http.Redirect(w, r, "/brands", http.StatusSeeOther)
 	}
 }
