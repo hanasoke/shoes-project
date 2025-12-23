7f6567027155bc233770cf6a409dae13d63edee4
hanasbp <hanasoke@gmail.com>
2025-12-23 22:46:52 +0700

    updated Add controller in shoecontroller.go

--- controllers/shoecontroller/shoecontroller.go
+++ controllers/shoecontroller/shoecontroller.go
@@ -3,7 +3,10 @@ package shoecontroller
 import (
 	"html/template"
 	"net/http"
+	"shoes-project/entities"
 	"shoes-project/models/shoemodel"
+	"strconv"
+	"time"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
@@ -30,5 +33,32 @@ func Add(w http.ResponseWriter, r *http.Request) {
 		}
 
 		temp.Execute(w, nil)
+	}
+
+	if r.Method == "POST" {
+		var shoe entities.Shoe
+
+		brandId, err := strconv.Atoi(r.FormValue("brand_id"))
+		if err != nil {
+			panic(err)
+		}
+
+		stock, err := strconv.Atoi(r.FormValue("shoe_stock"))
+		if err != nil {
+			panic(err)
+		}
+
+		shoe.Name = r.FormValue("shoe_name")
+		shoe.Brand.Brand_Id = uint(brandId)
+		shoe.Stock = int64(stock)
+		shoe.Description = r.FormValue("shoe_description")
+		shoe.CreatedAt = time.Now()
+
+		if ok := shoemodel.Create(shoe); !ok {
+			http.Redirect(w, r, r.Header.Get("Referer"), http.StatusTemporaryRedirect)
+			return
+		}
+
+		http.Redirect(w, r, "/shoes", http.StatusSeeOther)
 	}
 }
