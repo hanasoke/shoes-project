b1733f2bc6dc847f38c1e9317e1b53233feed3d8
hanasbp <hanasoke@gmail.com>
2025-12-25 18:33:34 +0700

    back to basic

--- controllers/shoecontroller/shoecontroller.go
+++ controllers/shoecontroller/shoecontroller.go
@@ -1,19 +1,9 @@
 package shoecontroller
 
 import (
-	"html/template"
 	"net/http"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	funcMap := template.FuncMap{
-		"add": func(a, b int) int {
-			return a + b
-		},
-	}
 
-	t := template.New("index.html").Funcs(funcMap)
-	t = template.Must(t.ParseFiles("views/shoes/index.html"))
-
-	t.Execute(w, nil)
 }
--- models/shoemodel/shoemodel.go
+++ models/shoemodel/shoemodel.go
@@ -1,51 +1,13 @@
 package shoemodel
 
-// func GetAll() []entities.Shoe {
-// 	rows, err := config.DB.Query(`
-// 		SELECT
-// 			shoes.id,
-// 			shoes.name,
-// 			shoes.type,
-// 			shoes.description,
-// 			shoes.sku,
-// 			shoes.price,
-// 			shoes.stock,
-// 			shoes.created_at,
-// 			shoes.updated_at
-// 		FROM shoes
-// 		JOIN brands ON shoes.brand_id = brands.id
-// 	`)
+import (
+	"shoes-project/entities"
+)
 
-// 	if err != nil {
-// 		panic(err)
-// 	}
+func GetAll() []entities.Shoe {
 
-// 	defer rows.Close()
-
-// 	var shoes []entities.Shoe
-
-// 	for rows.Next() {
-// 		var shoe entities.Shoe
-// 		err := rows.Scan(
-// 			&shoe.Id,
-// 			&shoe.Name,
-// 			&shoe.Type,
-// 			&shoe.Description,
-// 			&shoe.Sku,
-// 			&shoe.Price,
-// 			&shoe.Stock,
-// 			&shoe.CreatedAt,
-// 			&shoe.UpdatedAt,
-// 		)
-
-// 		if err != nil {
-// 			panic(err)
-// 		}
-
-// 		shoes = append(shoes, shoe)
-// 	}
-// 	return shoes
-// }
+	return nil
+}
 
 // func Create(shoe entities.Shoe) bool {
 // 	result, err := config.DB.Exec(`
