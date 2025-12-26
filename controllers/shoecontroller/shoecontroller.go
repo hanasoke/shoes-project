dac781163b9ef6c982a8ec9ba6fd6fdbd1dabe2e
hanasbp <hanasoke@gmail.com>
2025-12-26 11:16:34 +0700

    added add function in shoecontroller & shoebrand

--- controllers/shoecontroller/shoecontroller.go
+++ controllers/shoecontroller/shoecontroller.go
@@ -3,8 +3,11 @@ package shoecontroller
 import (
 	"html/template"
 	"net/http"
+	"shoes-project/entities"
 	"shoes-project/models/brandmodel"
 	"shoes-project/models/shoemodel"
+	"strconv"
+	"time"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
@@ -34,5 +37,41 @@ func Add(w http.ResponseWriter, r *http.Request) {
 		}
 
 		temp.Execute(w, data)
+	}
+
+	if r.Method == "POST" {
+		var shoe entities.Shoe
+
+		brandId, err := strconv.Atoi(r.FormValue("id_brand"))
+		if err != nil {
+			panic(err)
+		}
+
+		stock, err := strconv.Atoi(r.FormValue("stock"))
+		if err != nil {
+			panic(err)
+		}
+
+		price, err := strconv.Atoi(r.FormValue("price"))
+		if err != nil {
+			panic(err)
+		}
+
+		shoe.Name = r.FormValue("name")
+		shoe.Brand.Id = uint(brandId)
+		shoe.Type = r.FormValue("type")
+		shoe.Description = r.FormValue("description")
+		shoe.SKU = r.FormValue("sku")
+		shoe.Price = int64(price)
+		shoe.Stock = int64(stock)
+		shoe.CreatedAt = time.Now()
+		shoe.UpdatedAt = time.Now()
+
+		if ok := shoemodel.Create(shoe); !ok {
+			http.Redirect(w, r, r.Header.Get("Referer"), http.StatusTemporaryRedirect)
+			return
+		}
+
+		http.Redirect(w, r, "/shoes", http.StatusSeeOther)
 	}
 }
